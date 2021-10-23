f6a6d1039ad217337a160a94a6b9f50ac2093efd
Thushal Madhushankha <thushal98@gmail.com>
2021-10-23 11:33:39 +0530

    Initial Commit

---
+++ ihtMadhushankha-Cobsccomp005/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ihtMadhushankha-Cobsccomp005
+//
+//  Created by Thushal Madhushankha on 2021-10-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ihtMadhushankha-Cobsccomp005/ihtMadhushankha_Cobsccomp005App.swift
@@ -0,0 +1,17 @@
+//
+//  ihtMadhushankha_Cobsccomp005App.swift
+//  ihtMadhushankha-Cobsccomp005
+//
+//  Created by Thushal Madhushankha on 2021-10-23.
+//
+
+import SwiftUI
+
+@main
+struct ihtMadhushankha_Cobsccomp005App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
